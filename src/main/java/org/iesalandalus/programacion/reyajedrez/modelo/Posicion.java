85dcda3dcc55c0f82030fedaaea568070aaaa62f
Justo <justolm@gmail.com>
2023-12-04 20:31:28 +0100

    Creado el equals y hashcode

--- src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Posicion.java
@@ -1,5 +1,7 @@
 package org.iesalandalus.programacion.reyajedrez.modelo;
 
+import java.util.Objects;
+
 public class Posicion {
     private int fila;
     private char columna;
@@ -34,5 +36,18 @@ public class Posicion {
     public Posicion(Posicion posicion) {
         this.fila=posicion.fila;
         this.columna=posicion.columna;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Posicion posicion = (Posicion) o;
+        return fila == posicion.fila && columna == posicion.columna;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fila, columna);
     }
 }
