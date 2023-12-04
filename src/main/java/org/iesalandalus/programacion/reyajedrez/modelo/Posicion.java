4415ea94ef1fd8a888927a5d8a170140c5203dfc
Justo <justolm@gmail.com>
2023-12-04 20:44:20 +0100

    Finalizamos la Posicion creando el toString (ya verificado sin errores)

--- src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Posicion.java
@@ -12,7 +12,7 @@ public class Posicion {
 
     public void setFila(int fila) {
         if (fila < 1 || fila > 8){
-            throw new IllegalArgumentException("ERROR: Fila no válida.")
+            throw new IllegalArgumentException("ERROR: Fila no válida.");
         }
         this.fila = fila;
     }
@@ -23,7 +23,7 @@ public class Posicion {
 
     public void setColumna(char columna) {
         if (columna < 'a' || columna > 'h'){
-            throw new IllegalArgumentException("ERROR: Columna no válida.")
+            throw new IllegalArgumentException("ERROR: Columna no válida.");
         }
         this.columna = columna;
     }
@@ -34,6 +34,9 @@ public class Posicion {
     }
 
     public Posicion(Posicion posicion) {
+        if (posicion==null){
+            throw new NullPointerException("ERROR: No es posible copiar una posición nula.");
+        }
         this.fila=posicion.fila;
         this.columna=posicion.columna;
     }
@@ -53,8 +56,7 @@ public class Posicion {
 
     @Override
     public String toString() {
-        return "(fila=" + fila +
-                ", columna=" + columna +
-                ')';
+        return "fila=" + fila +
+                ", columna=" + columna;
     }
 }
