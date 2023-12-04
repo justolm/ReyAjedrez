5e774f9d167c08a74f4aa5bb3cac6d30176fe589
Justo <justolm@gmail.com>
2023-12-04 20:34:39 +0100

    Finalizamos la Posicion creando el toString

--- src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Posicion.java
@@ -50,4 +50,11 @@ public class Posicion {
     public int hashCode() {
         return Objects.hash(fila, columna);
     }
+
+    @Override
+    public String toString() {
+        return "(fila=" + fila +
+                ", columna=" + columna +
+                ')';
+    }
 }
