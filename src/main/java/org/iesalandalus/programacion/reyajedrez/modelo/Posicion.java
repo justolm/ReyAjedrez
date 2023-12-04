fbda985ea33a7935bb5d16f05900f06655ef07e9
Justo <justolm@gmail.com>
2023-12-04 20:17:06 +0100

    Creadas las excepciones de fila y columna no válidas

--- src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Posicion.java
@@ -4,4 +4,25 @@ public class Posicion {
     private int fila;
     private char columna;
 
+    public int getFila() {
+        return fila;
+    }
+
+    public void setFila(int fila) {
+        if (fila < 1 || fila > 8){
+            throw new IllegalArgumentException("ERROR: Fila no válida.")
+        }
+        this.fila = fila;
+    }
+
+    public char getColumna() {
+        return columna;
+    }
+
+    public void setColumna(char columna) {
+        if (columna < "a" || columna > "h"){
+            throw new IllegalArgumentException("ERROR: Columna no válida.")
+        }
+        this.columna = columna;
+    }
 }
