70ccd1efbe93a7938b00867eb0d6decc8aa18558
Justo <justolm@gmail.com>
2023-12-06 18:55:53 +0100

    Creamos el menú en la Consola.

--- src/main/java/org/iesalandalus/programacion/reyajedrez/Consola.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/Consola.java
@@ -3,4 +3,11 @@ package org.iesalandalus.programacion.reyajedrez;
 public class Consola {
     private Consola() {
     }
+    public void mostrarMenu(){
+        System.out.println("Opciones del menú:");
+        System.out.println("1. Crear rey por defecto.");
+        System.out.println("2. Crear rey eligiendo color.");
+        System.out.println("3. Mover.");
+        System.out.println("4. Salir.");
+    }
 }
