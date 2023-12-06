4faefd6e3e4de744cc8450bdbd2a4915990daeb6
Justo <justolm@gmail.com>
2023-12-06 14:45:40 +0100

    Creamos el toString del Rey

--- src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Rey.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Rey.java
@@ -1,5 +1,7 @@
 package org.iesalandalus.programacion.reyajedrez.modelo;
 
+import javax.naming.OperationNotSupportedException;
+
 public class Rey {
     public Color color;
     public Posicion posicion;
@@ -13,7 +15,7 @@ public class Rey {
         if (color==null){
             throw new RuntimeException("ERROR: El color no puede ser nulo.");
         }
-        if (!color.equals("Blanco")  && !color.equals("Negro")){
+        if (!color.equals(Color.BLANCO)  && !color.equals(Color.NEGRO)){
             throw new IllegalArgumentException("El color no es el esperado.");
         }
         this.color = color;
@@ -45,5 +47,72 @@ public class Rey {
         setColor(Color.BLANCO);
         setPosicion(new Posicion(1, 'e'));
         this.totalMovimientos = 0;
+    }
+
+    public Rey(Color color) {
+        if (color==null){
+            throw new NullPointerException("ERROR: El color no puede ser nulo.");
+        }
+        if (color.equals(Color.BLANCO)){
+            setColor(Color.BLANCO);
+            setPosicion(new Posicion(1, 'e'));
+        }
+        else if (color.equals(Color.NEGRO)){
+            setColor(Color.NEGRO);
+            setPosicion(new Posicion(8, 'e'));
+        }
+        this.totalMovimientos = 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Rey{" +
+                "color=" + color +
+                ", posicion=" + posicion +
+                '}';
+    }
+
+    public void mover (Direccion direccion) throws OperationNotSupportedException {
+        if (direccion == null){
+            throw new NullPointerException("ERROR: La dirección no puede ser nula.");
+        }
+        if (direccion.equals(Direccion.ENROQUE_LARGO) || direccion.equals(Direccion.ENROQUE_CORTO)){
+            if (totalMovimientos==0){
+                totalMovimientos++;
+                if (direccion.equals(Direccion.ENROQUE_CORTO)){
+                    posicion.setColumna('g');
+                }
+                else {
+                    posicion.setColumna('c');
+                }
+            }
+            else {
+                throw new OperationNotSupportedException("ERROR: El rey ya se ha movido antes.");
+            }
+        }
+        switch (direccion){
+            case SUR -> posicion.setFila(-1);
+            case SURESTE -> {
+                posicion.setFila(-1);
+                posicion.setColumna((char) +1);
+            }
+            case SUROESTE -> {
+                posicion.setFila(-1);
+                posicion.setColumna((char) -1);
+            }
+            case NORTE -> posicion.setFila(+1);
+            case NORESTE -> {
+                posicion.setFila(+1);
+                posicion.setColumna((char) +1);
+            }
+            case NOROESTE -> {
+                posicion.setFila(+1);
+                posicion.setColumna((char) -1);
+            }
+            case ESTE -> posicion.setColumna((char) +1);
+            case OESTE -> posicion.setColumna((char) -1);
+        }
+        // Falta crear el error si el rey se intenta mover fuera del tablero.
+
     }
 }
