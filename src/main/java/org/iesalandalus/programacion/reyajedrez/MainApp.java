cf7415b0103caaf63143de31acc2f867b90646d8
Justo <justolm@gmail.com>
2023-12-12 18:21:15 +0100

    Creado el atributo del rey

--- src/main/java/org/iesalandalus/programacion/reyajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/MainApp.java
@@ -1,7 +1,15 @@
 package org.iesalandalus.programacion.reyajedrez;
 
+import org.iesalandalus.programacion.reyajedrez.modelo.Color;
+import org.iesalandalus.programacion.reyajedrez.modelo.Posicion;
+
 public class MainApp {
     public static void main(String[] args) {
 
+    }
+    public static class rey {
+        private static Color color;
+        private Posicion posicion;
+        private int totalMovimientos;
     }
 }
