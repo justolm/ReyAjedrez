6011439d6a582073d40396385e438869c7b094be
Justo <justolm@gmail.com>
2023-12-12 18:32:22 +0100

    Creado elegirOpcionMenu

--- src/main/java/org/iesalandalus/programacion/reyajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/MainApp.java
@@ -3,6 +3,8 @@ package org.iesalandalus.programacion.reyajedrez;
 import org.iesalandalus.programacion.reyajedrez.modelo.Color;
 import org.iesalandalus.programacion.reyajedrez.modelo.Posicion;
 
+import static org.iesalandalus.programacion.reyajedrez.Consola.elegirOpcionMenu;
+
 public class MainApp {
     public static void main(String[] args) {
 
@@ -11,5 +13,15 @@ public class MainApp {
         private static Color color;
         private Posicion posicion;
         private int totalMovimientos;
+    }
+    public int ejecutarOpcion(){
+        elegirOpcionMenu();
+        switch (opcion){
+            case 1 -> // Crear el rey por defecto
+            case 2 -> // Crear el rey eligiendo el color
+            case 3 -> // Mover
+            case 4 -> // Salir
+            default -> throw new IllegalStateException("Ha elegido una opción no válida: " + opcion);
+        }
     }
 }
